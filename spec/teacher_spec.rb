1f547641e10bd7db51b1782ab43df40230b9d569
aron-helu <aronmilahela@gmail.com>
2022-10-14 00:25:24 +0300

    add unit test to teacher

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../teacher'
+
+describe Teacher do
+    before (:each) do
+        @teacher = Teacher.new('Developer', 5, 'Tiwa', parent_permission = true )
+    end
+
+    it 'Should be create a student name' do
+        expect(@teacher.name).to eq "Tiwa"
+    end
+
+    it 'Should be create a student name' do
+        expect(@teacher.specialization).to eq "Developer"
+    end
+
+end    
